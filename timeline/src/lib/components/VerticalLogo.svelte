dcdcaa197ca54c4cdf87e6aeee3eff12243074c0
Jait Jacob <jai8.jacob@gmail.com>
2024-03-08 02:19:42 +0530

    add a Vertical Logo component

---
+++ timeline/src/lib/components/VerticalLogo.svelte
@@ -0,0 +1,6 @@
+<script>
+  import lean_logo from '$lib/assets/logo_without_text.svg'
+</script>
+
+<img class="size-9 float-right" alt="logo without text" src={lean_logo} />
+<p class="text-slate-950 font-bold text-base text-right">Duggup</p>
